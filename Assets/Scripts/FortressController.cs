a1c9341b94349cb36b7d767933cae69f3fab72aa
Connor McCauley <cdmc13@outlook.com>
2020-10-07 23:49:39 -0400

    fortress properties and debugging check

--- Assets/Scripts/FortressController.cs
+++ Assets/Scripts/FortressController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class FortressController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // When the fortress is destroyed, the team loses the game.
+    public double currentHealth = 0;
+    public double maxHealth = 500.0;
+    // If all(any?) max tier towers are still standing, the fortress is invulnerable.
+    public bool isProtected = true;
+    // The armour value reduces physical damage taken by a set amount per point as defined in the game manager.
+    public int baseArmour = 10;
 
-    // Update is called once per frame
-    void Update()
+    // Use this for initialization
+    void Start() 
     {
-        
+        currentHealth = maxHealth;
+        isProtected = true;
+        Debug.Log("This fortress is active, with " +  currentHealth + '/' + maxHealth + " health.");
     }
 }
